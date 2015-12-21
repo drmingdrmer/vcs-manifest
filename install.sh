8dcdf2d5c1a6e94c0ddc10e280a907598e325833
drdr xp <drdr.xp@gmail.com>
2015-12-21 15:21:03 +0800

    add instll.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+
+# https://github.com/esrlabs/git-repo
+
+mkdir ~/bin
+curl https://raw.githubusercontent.com/esrlabs/git-repo/master/repo > ~/bin/repo
+curl https://raw.githubusercontent.com/esrlabs/git-repo/master/repo.cmd > ~/bin/repo.cmd
