2d3bcb3cff7f18fe9320656682305a8d555a604e
drdr xp <drdr.xp@gmail.com>
2018-08-05 13:57:49 +0800

    A  make-it.sh

---
+++ make-it.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# require [sudo] npm install -g gh
+
+# gh repo -l -u baishancloud
+
+
+{
+    grep "^baishancloud/" \
+
+}
+<<-END
+Listing owner repos for baishancloud
+baishancloud/ant-design-bsc
+forks: 0, stars: 1, issues: 0
+
+baishancloud/baishancloud.github.io
+forks: 6, stars: 2, issues: 0
+
+baishancloud/ban
+forks: 1, stars: 0, issues: 0
+
+baishancloud/bsyopensource
+forks: 0, stars: 0, issues: 0
+
+baishancloud/EzSVG
+forks: 0, stars: 0, issues: 0
+END
